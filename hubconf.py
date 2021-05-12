aa674eb2821712ae501e3010b33a70d2e111e905
eliot <lamboroteliot@gmail.com>
2021-05-12 09:53:46 +0200

    Create hubconf.py

---
+++ hubconf.py
@@ -0,0 +1,12 @@
+dependencies = ['torch']
+from torchvision.models.resnet import resnet18 as _resnet18
+
+# resnet18 is the name of entrypoint
+def resnet18(pretrained=False, **kwargs):
+    """ # This docstring shows up in hub.help()
+    Resnet18 model
+    pretrained (bool): kwargs, load pretrained weights into the model
+    """
+    # Call the model, load pretrained weights
+    model = _resnet18(pretrained=pretrained, **kwargs)
+    return model
