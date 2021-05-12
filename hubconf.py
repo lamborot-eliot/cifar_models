f3e92cfc3a95995731588887d48969b5ea9a710c
eliot <lamboroteliot@gmail.com>
2021-05-12 10:23:29 +0200

    update name to lower case

--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,6 @@
 dependencies = ['torch']
 from torchvision.models.resnet import resnet18 as _resnet18
-from charles.src.effnet_models.mobilenetv2_dense import MobileNetV2
+from charles.src.effnet_models.mobilenetv2_dense import MobileNetV2 as _mobilenetv2
 import torch
 
 # resnet18 is the name of entrypoint
@@ -13,18 +13,19 @@ def resnet18(pretrained=False, **kwargs):
     model = _resnet18(pretrained=pretrained, **kwargs)
     return model
 
-def robustMobileNetV2(pretrained=False, **kwargs):
+def mobilenetv2(pretrained=False, **kwargs):
     """ # This docstring shows up in hub.help()
     robustMobileNetV2 model
     pretrained (bool): kwargs, load pretrained weights into the model
     """
     # Call the model, load pretrained weights
-    model = MobileNetV2()
-    model.load_state_dict(torch.load('charles/cifar10_models/mobilenet_pgd_dense.pth'))
+    model = _mobilenetv2(**kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load('charles/cifar10_models/mobilenet_pgd_dense.pth'))
     return model
 
 
 
 if __name__=="__main__":
-    model = robustMobileNetV2()
+    model = mobilenetv2(pretrained=True)
     print(model.__class__)
--- test.py
+++ test.py
@@ -2,3 +2,5 @@ import torch
 
 model = torch.hub.load("lamborot-eliot/cifar_models:main", "resnet18", pretrained=False)
 print(model.__class__)
+model = torch.hub.load("lamborot-eliot/cifar_models:main", "mobilenetv2", pretrained=False)
+print(model.__class__)
