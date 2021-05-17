3737fcdac2e67ae5b8058d2b612e973ff87eacae
eliot <lamboroteliot@gmail.com>
2021-05-17 15:27:13 +0200

    add ACL resnet18 model

--- hubconf.py
+++ hubconf.py
@@ -3,6 +3,8 @@ from torchvision.models.resnet import resnet18 as _resnet18
 from charles.src.effnet_models.mobilenetv2_dense import MobileNetV2 as _mobilenetv2
 from charles.src.resnet_model.resnet_dense import ResNet34 as _resnet34
 
+from ACL.models.resnet import resnet18 as _aclResnet18
+
 import torch
 import os
 
@@ -32,7 +34,28 @@ def resnet34(pretrained=False, **kwargs):
         model.load_state_dict(torch.load(checkpoint))
     return model
 
+def resnet18ACL(pretrained=False, **kwargs):
+    """ # This docstring shows up in hub.help()
+    acl resnet18 model
+    pretrained (bool): kwargs, load pretrained weights into the model
+    """
+    # Call the model, load pretrained weights
+    model = _aclResnet18(num_classes=10, **kwargs)
+    if pretrained:
+        dirname = os.path.dirname(__file__)
+        checkpoint = os.path.join(dirname, './ACL/downloaded_checkpoints/ACL_DS_TUNE.pt')
+        checkpoint = torch.load(checkpoint, map_location="cpu")
+        if 'state_dict' in checkpoint:
+            state_dict = checkpoint['state_dict']
+        elif 'P_state' in checkpoint:
+            state_dict = checkpoint['P_state']
+        else:
+            state_dict = checkpoint
+
+        model.load_state_dict(state_dict)
+    return model
+
 
 if __name__=="__main__":
-    model = resnet34(pretrained=True)
+    model = resnet18ACL(pretrained=True)
     print(model.__class__)
